2ce10963de28f21763fe2857982465e2314d0899
Sebastian Ebel <sebastianebel@hotmail.de>
2021-11-21 15:10:30 +0100

    current player change color

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = ['Alex', 'Janis', 'Elias'];
+    public players: string[] = [];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentplayer: number = 0;
